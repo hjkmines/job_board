000599c8ad383605066fd6ef1645adfc8920d1ce
Chris Medyna <chris.medyna@gmail.com>
2023-02-16 17:12:57 -0500

    svg errors

---
+++ client/src/components/FillerSpace.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const FillerSpace = () => {
+  return (
+    <>
+        <div>
+            Filler
+        </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        <div>
+            Filler
+        </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        <div>
+            Filler
+        </div>
+        <br/>
+            <br/>
+            <br/>
+            <br/>
+        <div>
+            Filler
+        </div>
+            <br/>
+            <br/>
+            <br/>
+        <div>
+            Filler
+        </div>
+            <br/>
+            <br/>
+            <br/>
+        <div>
+            Filler
+        </div>
+    </>
+  )
+}
+
+export default FillerSpace
