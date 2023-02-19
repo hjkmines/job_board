831092dfba326fb00cc42881cec1a10ae0b9d5b1
Vinh Peters <vinhpeters@outlook.com>
2023-02-19 15:30:20 -0500

    Add styles to match Figma design

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,48 +1,54 @@
-import React from 'react';
+import './JobCard.css'
 import {
   MDBCard,
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
-  MDBCardHeader,
-  MDBCardFooter,
   MDBBtn,
-  MDBListGroupItem,
+  MDBRow,
+  MDBCol,
+  MDBContainer,
   MDBCardSubTitle
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 
 export default function App() {
   return (
-    <MDBContainer>
-    <MDBCard>
-      <MDBCardBody>
-        <MDBCardTitle className="h2">Job Title</MDBCardTitle>
-        <MDBCardSubTitle>Company</MDBCardSubTitle>
-        <MDBCardText>
-          <ul>
-            <li>
-              Candidate will work as Front-End Developer utilizing SDLC
-              process
-            </li>
-            <li>Proficient in JavaScript and TypeScript</li>
-            <li>
-              Familiar with AWS core technology such as Lambda, S3, Cloud
-              formation, SQS
-            </li>
-          </ul>
-          We are looking for an Intermediate Software Engineer to join our
-          development team that builds and maintains high-traffic storefronts
-          on the Shopify platform. The Technology team supports a dynamic end
-          to end e-commerce organization and implements a variety of
-          technology partners as well as custom solutions and integrations.
-          The right candidate for the role is a team player and problem solver
-          who enjoys learning and implementing new systems and technologies.{" "}
-        </MDBCardText>
-        <MDBBtn href="#" rounded className="shadow-none">
-          Apply
-        </MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
-  </MDBContainer>
+    <MDBContainer className="m-4 bg-transparent border">
+      <MDBRow>
+        <MDBCol sm="8">
+          <MDBCard className="job-card p-1">
+            <MDBCardBody>
+              <MDBCardTitle className="job-title mb-0">Job Title</MDBCardTitle>
+              <MDBCardSubTitle>Company</MDBCardSubTitle>
+              <ul>
+                <li>
+                  Candidate will work as Front-End Developer utilizing SDLC
+                  process
+                </li>
+                <li>Proficient in JavaScript and TypeScript</li>
+                <li>
+                  Familiar with AWS core technology such as Lambda, S3, Cloud
+                  formation, SQS
+                </li>
+              </ul>
+
+              <MDBCardText className="mt-2 job-text">
+                We are looking for an Intermediate Software Engineer to join our
+                development team that builds and maintains high-traffic
+                storefronts on the Shopify platform. The Technology team
+                supports a dynamic end to end e-commerce organization and
+                implements a variety of technology partners as well as custom
+                solutions and integrations. The right candidate for the role is
+                a team player and problem solver who enjoys learning and
+                implementing new systems and technologies.
+              </MDBCardText>
+              <MDBBtn href="#" rounded className="shadow-none apply-btn">
+                Apply
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
