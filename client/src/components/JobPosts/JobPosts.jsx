78b0dc2161755953e5c479987f3f195a899ad4df
Qibin <qbye0405@gmail.com>
2023-02-18 20:08:10 -0500

    created job posts container

--- client/src/components/JobPosts/JobPosts.jsx
+++ client/src/components/JobPosts/JobPosts.jsx
@@ -1,12 +1,100 @@
 import React from 'react';
 import "./JobPosts.css";
 
+
 const JobPosts = () => {
   return (
-    <>
-        <div>JobPosts</div>
+    // parent container - center everything 
+    <div className='container-fluid' id="jobPosts">
+
+      {/* // section 1 - your matches  // */}
+        <div className='row my-3'>
+          <div>Your Matches</div>
+          <div className='col-4'>
+            <p>Job 1</p>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary">Button</button>
+              </div>
+            </div>
+          </div>
         
-    </>
+
+          <div className='col-4'>
+            <div>
+            <p>Job 2</p>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button type="button" class="btn btn-primary">Button</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-4'>
+            <div>
+            <p> Job 3 </p>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button type="button" class="btn btn-primary">Button</button>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <hr class="hr hr-blurry my-3" />
+
+      {/* Section 2 -- Local jobs (NYC) */}
+        <div className='row my-3'>
+        <div>Local jobs</div>
+          <div className='col-4'>
+            <p>Job 1</p>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary">Button</button>
+              </div>
+            </div>
+          </div>
+        
+
+          <div className='col-4'>
+            <div>
+            <p>Job 2</p>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button type="button" class="btn btn-primary">Button</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-4'>
+            <div>
+            <p> Job 3 </p>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button type="button" class="btn btn-primary">Button</button>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+
+
+    </div>
+ </div>
   )
 }
 
