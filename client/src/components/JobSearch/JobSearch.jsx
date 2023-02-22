ad49721a043afe3f1165af746ef635b6bf6118d7
Chris Medyna <chris.medyna@gmail.com>
2023-02-22 13:08:56 -0500

    test

--- client/src/components/JobSearch/JobSearch.jsx
+++ client/src/components/JobSearch/JobSearch.jsx
@@ -1,3 +1,5 @@
+import './JobSearch.css';
+import React, { useState } from 'react'
 import { 
     MDBCol,
     MDBContainer, 
@@ -9,64 +11,124 @@ import {
     MDBDropdownItem,
     MDBInput,
     MDBBtn,
-    MDBIcon
-
+    MDBIcon,
+    MDBTypography,
+    MDBInputGroup
 } from 'mdb-react-ui-kit'
-import React from 'react'
-import './JobSearch.css';
+import CreateableSelect from 'react-select/creatable';
+import Select from 'react-select';
+
+
+
+const cityOptions = [
+    {value:"New York", label:"New York"},
+    {value:"Boston", label:"Boston"},
+    {value:"Washington D.C.", label:"Washington, D.C."},
+    {value:"Los Angeles", label:"Los Angeles"},
+    {value:"San Fransisco", label:"San Fransisco"},
+    {value:"Houston", label:"Houston"},
+    {value:"Dallas", label:"Dallas"},
+    {value:"Portland", label:"Portland"},
+    {value:"Seattle", label:"Seattle"},
+];
+
+
+const stateOptions = [
+    {value:"Alabama",label:"AL"},{value:"Alaska",label:"AK"},{value:"Arizona",label:"AZ"},{value:"Arkansas",label:"AR"},{value:"California",label:"CA"},{value:"Colorado",label:"CO"},{value:"Connecticut",label:"CT"},{value:"Delaware",label:"DE"},{value:"Florida",label:"FL"},{value:"Georgia",label:"GA"},{value:"Hawaii",label:"HI"},{value:"Idaho",label:"ID"},{value:"Illinois",label:"IL"},{value:"Indiana",label:"IN"},{value:"Iowa",label:"IA"},{value:"Kansas",label:"KS"},{value:"Kentucky",label:"KY"},{value:"Louisiana",label:"LA"},{value:"Maine",label:"ME"},{value:"Maryland",label:"MD"},{value:"Massachusetts",label:"MA"},{value:"Michigan",label:"MI"},{value:"Minnesota",label:"MN"},{value:"Mississippi",label:"MS"},{value:"Missouri",label:"MO"},{value:"Montana",label:"MT"},{value:"Nebraska",label:"NE"},{value:"Nevada",label:"NV"},{value:"New Hampshire",label:"NH"},{value:"New Jersey",label:"NJ"},{value:"New Mexico",label:"NM"},{value:"New York",label:"NY"},{value:"North Carolina",label:"NC"},{value:"North Dakota",label:"ND"},{value:"Ohio",label:"OH"},{value:"Oklahoma",label:"OK"},{value:"Oregon",label:"OR"},{value:"Pennsylvania",label:"PA"},{value:"Rhode Island",label:"RI"},{value:"South Carolina",label:"SC"},{value:"South Dakota",label:"SD"},{value:"Tennessee",label:"TN"},{value:"Texas",label:"TX"},{value:"Utah",label:"UT"},{value:"Vermont",label:"VT"},{value:"Virginia",label:"VA"},{value:"Washington",label:"WA"},{value:"West Virginia",label:"WV"},{value:"Wisconsin",label:"WI"},{value:"Wyoming",label:"WY"}]
+
+    const experienceOptions = [
+        {value: "0", label:"0 - no experience"},
+        {value: "1", label:"internship ~ 6 months"},
+        {value: "2", label:"1 year"},
+        {value: "3", label:"2 years"},
+    ]
+
+    const sortByOptions = [
+        {value:"remote", label:"Remote"},
+        {value:"location", label:"Location"},
+        {value:"experience", label:"Experience"},
+
+    ]
+
+    const handleSearch= (event) => {
+        console.log(event.target.data)
+    }
+
 
 export default function App () {
+
+
   return (
-    <MDBContainer fluid className='bg-white' id="searchFilterContainer"  >
-        <MDBRow className='py-2' id='searchRow'>
-            <MDBCol >
-                    <p>Filters: </p>
+    <MDBContainer fluid className='search2'>
+    <form className='searchForm'>
+        <MDBRow className='searchRow my-2 mr-0 gl-5 gr-0'>
+            <MDBCol size={2} className='searchBtnCol g-0 '>
+               <MDBBtn type="submit" className='searchSubmitBtn' onSubmit={handleSearch}>
+                    Search
+               </MDBBtn>
             </MDBCol>
-            <MDBCol >
-                    <MDBSwitch id="remoteWork" label="Remote Work" />
-            </MDBCol>
-            <MDBCol >
-                <MDBInput label="City" id="cityInput" type="text" />
-            </MDBCol>
-            <MDBCol >
-                <MDBDropdown className="btn-group" id="stateButton">
-                    <MDBDropdownToggle>
-                    State
-                    </MDBDropdownToggle>
-                    <MDBDropdownMenu>
-                        <MDBDropdownItem >NY</MDBDropdownItem>
-                        <MDBDropdownItem >CA</MDBDropdownItem>
-                        <MDBDropdownItem >TX</MDBDropdownItem>
-                    </MDBDropdownMenu>
-                </MDBDropdown>
-            </MDBCol>
-            <MDBCol >
-            <MDBDropdown className='btn' id="stateButton">
-                    <MDBDropdownToggle tag='a' role='button'>
-                    Experience
-                    </MDBDropdownToggle>
-                    <MDBDropdownMenu>
-                    <MDBDropdownItem >0 - no experience</MDBDropdownItem>
-                    <MDBDropdownItem >internship ~ 6 months</MDBDropdownItem>
-                    <MDBDropdownItem >1 year</MDBDropdownItem>
-                    <MDBDropdownItem >2 years</MDBDropdownItem>
-                    </MDBDropdownMenu>
-                </MDBDropdown>
-            </MDBCol>
-            <MDBCol >
-                <MDBDropdown>
-                    <MDBDropdownToggle>Sort By:</MDBDropdownToggle>
-                    <MDBDropdownMenu>
-                        <MDBDropdownItem>Remote</MDBDropdownItem>
-                        <MDBDropdownItem>Location</MDBDropdownItem>
-                        <MDBDropdownItem>Experience</MDBDropdownItem>
-                    </MDBDropdownMenu>
-                </MDBDropdown>
-            </MDBCol>
-            <MDBCol >
-                <MDBIcon fas icon="ellipsis-h" size='2x' animate='beat-fade' />
+            <MDBCol size={10} className="filterSearch">
+                <MDBInput label="Search" type='text' className="searchInput"/>
             </MDBCol>
         </MDBRow>
+
+        <MDBRow fluid='true' className='searchRow2 mb-2'>
+            <MDBCol size={2}>
+                <MDBSwitch form="true" id="remoteWork" label="Remote Work" value={'Remote'} aria-label='remote work toggle'/>
+            </MDBCol>
+            <MDBCol size={2}>
+               <CreateableSelect 
+                isClearable='true'
+                // isDisabled={isLoading}
+                // isLoading={isLoading}
+                // onChange={(newValue) => setValue(newValue)}
+                // onCreateOption={handleCityCreate}
+                placeholder="City" 
+                className='CityLabel' 
+                name='city'
+                options={cityOptions} 
+                />
+            </MDBCol>
+            <MDBCol size={2}>
+                <Select 
+                    isClearable
+                    className='basic-single' 
+                    placeholder='State'  
+                    name="State"
+                     options={stateOptions} 
+                />
+            </MDBCol>
+            <MDBCol size={2}>
+                <Select  
+                    isClearable
+                    className='basic-single'
+                    classNamePrefix='select' 
+                    placeholder='experience' 
+                    name="Experience" 
+                    options={experienceOptions}
+                />
+            </MDBCol>
+            <MDBCol size={2}>
+                <Select className='basic-single' placeholder='sort by'name="sortBy" options={sortByOptions} />
+            </MDBCol>
+            <MDBCol size={2}>
+                <MDBDropdown className='moreDropDown' >
+                <MDBIcon icon="ellipsis-h" size='2x' color='dark' />
+                    <MDBDropdownToggle tag='a' className='moreFilter fas' icon="elipsis-h">
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu>
+                        <MDBDropdownItem>settings</MDBDropdownItem>
+                        <MDBDropdownItem>more and more</MDBDropdownItem>
+                        <MDBDropdownItem>advanced filter</MDBDropdownItem>
+                    </MDBDropdownMenu>
+
+                </MDBDropdown>
+            </MDBCol>
+        </MDBRow>
+            </form>
+        
     </MDBContainer>
+
+    // </MDBContainer>
   )
 }
