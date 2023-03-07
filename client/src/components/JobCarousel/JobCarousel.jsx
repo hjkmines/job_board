79769e4aeaeca66e78ba1de91313df4d07456d69
Vinh Peters <vinhpeters@outlook.com>
2023-03-07 17:23:30 -0500

    Match carousel styles to Figma design

--- client/src/components/JobCarousel/JobCarousel.jsx
+++ client/src/components/JobCarousel/JobCarousel.jsx
@@ -1,40 +1,122 @@
-import JobCard from "../JobCard/JobCard";
+
+import React from "react";
 import Carousel from "react-multi-carousel";
+import JobCard from "../JobCard/JobCard";
+import "./JobCarousel.css";
 import "react-multi-carousel/lib/styles.css";
-import './scrollbar.css';
 
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
 
+class JobCarousel extends React.Component {
+    state = { additionalTransfrom: 0 };
+    render() {
+        const { deviceType } = this.props;
+        const CustomSlider = ({ carouselState }) => {
+            let value = 0;
+            let carouselItemWidth = 0;
+            if (this.Carousel) {
+                carouselItemWidth = this.Carousel.state.itemWidth;
+                const maxTranslateX = Math.round(
+                    // so that we don't over-slide
+                    carouselItemWidth *
+                    (this.Carousel.state.totalItems -
+                        this.Carousel.state.slidesToShow) +
+                    150
+                );
+                value = maxTranslateX / 100; // calculate the unit of transform for the slider
+            }
+            const { transform, currentSlide } = carouselState;
+            return (
+                <div className="custom-slider">
+                    <input
+                        type="range"
+                        value={Math.round(Math.abs(transform) / value)}
+                        max={
+                            (carouselItemWidth *
+                                (carouselState.totalItems - carouselState.slidesToShow) +
+                                (this.state.additionalTransfrom === 150 ? 0 : 150)) /
+                            value
+                        }
+                        onChange={e => {
+                            if (this.Carousel.isAnimationAllowed) {
+                                this.Carousel.isAnimationAllowed = false;
+                            }
+                            const nextTransform = e.target.value * value;
+                            const nextSlide = Math.round(nextTransform / carouselItemWidth);
+                            if (
+                                e.target.value == 0 &&
+                                this.state.additionalTransfrom === 150
+                            ) {
+                                this.Carousel.isAnimationAllowed = true;
+                                this.setState({ additionalTransfrom: 0 });
+                            }
+                            this.Carousel.setState({
+                                transform: -nextTransform, // padding 20px and 5 items.
+                                currentSlide: nextSlide
+                            });
+                        }}
+                        className="custom-slider__input"
+                    />
+                    
+                </div>
+            );
+        };
+        return (
+            <Carousel
+                keyBoardControl={true}
+                ssr={false}
+                ref={el => (this.Carousel = el)}
+                partialVisible={false}
+                arrows={false}
+                customButtonGroup={<CustomSlider />}
+                deviceType={this.props.deviceType}
+                itemClass="job-item"
+                itemAriaLabel="Image-aria-label"
+                responsive={responsive}
+                containerClass="carousel-container-with-scrollbar"
+                focusOnSelect={true}
+                additionalTransfrom={this.state.additionalTransfrom-50}
 
-export default function JobCarousel() {
+                beforeChange={nextSlide => {
+                    if (nextSlide !== 0 && this.state.additionalTransfrom !== 150) {
+                        this.setState({ additionalTransfrom: 150 });
+                    }
+                    if (nextSlide === 0 && this.state.additionalTransfrom === 150) {
+                        this.setState({ additionalTransfrom: 0 });
+                    }
+                }}
+            >
 
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                <JobCard />
 
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 3 // optional, default to 1.
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 2 // optional, default to 1.
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1 // optional, default to 1.
-        }
-    };
-    return (
-
-        <Carousel
-        responsive={responsive}
-        >
-            <JobCard/>
-            <JobCard/>
-            <JobCard/>
-            <JobCard/>
-            
-        </Carousel>
-)
+            </Carousel>
+        );
+    }
 }
+
+export default JobCarousel;
--- client/src/components/JobSection/JobSection.jsx
+++ client/src/components/JobSection/JobSection.jsx
@@ -2,30 +2,28 @@
 import {
     MDBContainer,
     MDBTypography,
-  
-  } from 'mdb-react-ui-kit';
-  
-  import "./JobSection.css";
-  
-  import WithScrollbar from '../JobCarousel/WithScrollbar';
-  
-  const JobSection = ({sectionTitle}) => {
-   
+
+} from 'mdb-react-ui-kit';
+
+import "./JobSection.css";
+import JobCarousel from '../JobCarousel/JobCarousel';
+
+const JobSection = ({ sectionTitle }) => {
+
     return (
-      <MDBContainer className='mb-4'>
-  
-        <MDBTypography tag='h2' className='pt-3 section-header'>
-          {sectionTitle}
-        </MDBTypography>
-  
-        <MDBContainer fluid className='m-2 overflow-hidden p-2 '>
-          <WithScrollbar />
+        <MDBContainer className='mb-4'>
+
+            <MDBTypography tag='h2' className='pt-3 section-header'>
+                {sectionTitle}
+            </MDBTypography>
+
+            <MDBContainer fluid className='m-2 overflow-hidden p-2 '>
+                <JobCarousel />
+            </MDBContainer>
+
         </MDBContainer>
-  
-      </MDBContainer>
-  
+
     )
-  }
-  
-  export default JobSection;
-  
+}
+
+export default JobSection;
