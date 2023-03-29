e4601ea6fd2f381d4f5cd25e3f2be301a2c3f1ba
Vinh Peters <vinhpeters@outlook.com>
2023-03-28 20:01:10 -0400

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express'); 
 const dotenv = require('dotenv'); 
 const connectDB = require('./config/db')
-const bodyParser = require('body-parser')
 const jobRouter = require('./routes/jobRouter'); 
+const cors = require("cors");
 
 dotenv.config({ path: './config/config.env' })
 
 connectDB(); 
 
 const app = express(); 
+app.use(cors({origin:true, credentials: true}))
 
-app.use(bodyParser.json())
+app.use(express.json())
+
 
 const PORT = process.env.PORT || 5001; 
 
